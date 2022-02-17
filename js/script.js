98c07fbdf1dd089b316d46f66a883e2991361157
forhadbinamir <forhadbinamir@gmail.com>
2022-02-17 19:29:29 +0300

    case error added

--- js/script.js
+++ js/script.js
@@ -1,13 +1,38 @@
-// get expenses total result here
-function getExpenses() {
+// food expenses function
+function foodExpenses() {
     const footExpenses = document.getElementById('food-expenses');
-    const footValue = parseFloat(footExpenses.value);
+    const foodValue = parseFloat(footExpenses.value);
+    if (foodValue > 0) {
+        return foodValue;
+    } else {
+        document.getElementById('warning').style.display = 'block';
+    }
+
+}
+// rent expanses function
+function rentExpenses() {
     const rentExpenses = document.getElementById('rent-expenses');
     const rentValue = parseFloat(rentExpenses.value);
+    if (rentValue > 0) {
+        return rentValue;
+    } else {
+        document.getElementById('warning').style.display = 'block';
+    }
+}
+// clothes expenses function
+function clotheExpenses() {
     const clotheExpenses = document.getElementById('clothe-expenses');
     const clotheValue = parseFloat(clotheExpenses.value);
+    if (clotheValue > 0) {
+        return clotheValue;
+    } else {
+        document.getElementById('warning').style.display = 'block';
+    }
+}
+// get expenses total result here
+function getExpenses() {
     //total monthly expenses
-    const totalExpense = footValue + rentValue + clotheValue;
+    const totalExpense = foodExpenses() + rentExpenses() + clotheExpenses();
     return totalExpense;
 }
 
@@ -15,7 +40,13 @@ function getBalance() {
     // total monthly income
     const monthlyIncome = document.getElementById('monthly-income');
     const monthlyValue = parseFloat(monthlyIncome.value);
-    return monthlyValue;
+    if (monthlyValue > 0) {
+        return monthlyValue;
+    } else {
+        document.getElementById('warning').style.display = 'block';
+
+    }
+
 }
 
 function totalSaveBalance() {
@@ -31,7 +62,6 @@ document.getElementById('calculate').addEventListener('click', function () {
     document.getElementById('total-expenses').innerText = getExpenses();
     document.getElementById('balance-Money').innerText = totalSaveBalance();
 
-
 });
 
 
@@ -39,8 +69,13 @@ document.getElementById('calculate').addEventListener('click', function () {
 
 document.getElementById('save-btn').addEventListener('click', function () {
     const percentageMoney = document.getElementById('percentage');
-    const percentageValue = percentageMoney.value;
-    const savingMoney = totalSaveBalance() / percentageValue;
+    const percentageValue = parseInt(percentageMoney.value);
+    const savingMoney = getBalance() / percentageValue;
+    if (savingMoney > totalSaveBalance()) {
+        document.getElementById('warning2').style.display = 'block';
+    } else {
+        document.getElementById('warning3').style.display = 'block';
+    }
     const remainigMoney = totalSaveBalance() - savingMoney;
     // saving amount show into the ui design
     document.getElementById('saving-amount').innerText = savingMoney;
